f22fa33fc89047ffefab52326ca3516d7577c582
syzer3@gmail.com <syzer3@gmail.com>
2017-12-04 21:12:55 +0100

    still pupeteer

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const puppeteer = require('puppeteer')
 
 ;(async () => {
-  const browser = await puppeteer.launch()
+  const browser = await puppeteer.launch({
+    headless: false,
+    slowMo: 250 // slow down by 250ms
+  })
   const page = await browser.newPage()
   await page.goto('https://google.com')
+
+  await page.evaluate(() => console.log(`url is ${location.href}`))
+
   await page.screenshot({ path: 'example.png' })
 
   await browser.close()
