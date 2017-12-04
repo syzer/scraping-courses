839e04fa4221c81173a4dcf72b4ee92565f8d2af
syzer3@gmail.com <syzer3@gmail.com>
2017-12-04 22:40:51 +0100

    cookies aproach with console.log :)

--- index.js
+++ index.js
@@ -1,17 +1,20 @@
 const dotenv = require('dotenv').config()
-const url = 'https://frontendmasters.com/courses/functional-javascript-v2/composition-introduction/'
+// const url = 'https://frontendmasters.com/courses/functional-javascript-v2/composition-introduction/'
+const url = 'https://frontendmasters.com/'
 const puppeteer = require('puppeteer')
 
-const loginKey = 'wordpress_logged_in_323a64690667409e18476e5932ed231e'
+// const loginKey = 'wordpress_logged_in_323a64690667409e18476e5932ed231e'
 const { COOKIES } = dotenv.parsed
 const cookies = COOKIES.split(';')
-.map(line => line.trim().split('='))
-.filter((arr) => arr.includes(loginKey))
-  .map(([name, value]) => ({
-    url,
+  .map(line => line.trim().split(/=/))
+  // .filter((arr) => arr.includes(loginKey))
+  .map(([name, ...values]) => ({
+    url: url,
     name,
-    value
+    value: values.join('='),
   }))
+  // .map(console.log)
+// throw 'nope'
 
 ;(async () => {
   const browser = await puppeteer.launch({
@@ -19,9 +22,11 @@ const cookies = COOKIES.split(';')
     slowMo: 550 // slow down by 250ms
   })
   const page = await browser.newPage()
-  await page.setCookie(cookies[0])
+
+  await Promise.all(cookies.map(cookie => page.setCookie(cookie)))
+
   await page.goto(url)
-  // TODO cookies
+
 
   const content = await page.content()
 
