ac7ed175e7be28aac7d666a3e0b5f0cb16c67cc5
syzer3@gmail.com <syzer3@gmail.com>
2017-12-05 02:22:40 +0100

    cleanup

--- index.js
+++ index.js
@@ -1,20 +1,15 @@
 const dotenv = require('dotenv').config()
-// const url = 'https://frontendmasters.com/courses/functional-javascript-v2/composition-introduction/'
 const url = 'https://frontendmasters.com/'
 const puppeteer = require('puppeteer')
 
-// const loginKey = 'wordpress_logged_in_323a64690667409e18476e5932ed231e'
 const { COOKIES } = dotenv.parsed
 const cookies = COOKIES.split(';')
   .map(line => line.trim().split(/=/))
-  // .filter((arr) => arr.includes(loginKey))
   .map(([name, ...values]) => ({
     url: url,
     name,
     value: values.join('='),
   }))
-  // .map(console.log)
-// throw 'nope'
 
 ;(async () => {
   const browser = await puppeteer.launch({
@@ -30,10 +25,5 @@ const cookies = COOKIES.split(';')
 
   const content = await page.content()
 
-  // console.log(content)
-  // await page.evaluate(() => console.log(`url is ${location.href}`))
-
-  // await page.screenshot({ path: 'example.png' })
-
-  // await browser.close()
+  console.log(content)
 })()
