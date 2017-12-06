dde329dba964d5eacb0b322319dff7449a9fd07a
syzer3@gmail.com <syzer3@gmail.com>
2017-12-06 20:17:48 +0100

    Refactoring.. totally worth it :)

--- index.js
+++ index.js
@@ -1,42 +1,8 @@
 const fs = require('fs')
 const fetch = require('node-fetch')
-const dotenv = require('dotenv').config()
-const puppeteer = require('puppeteer')
 const _ = require('lodash')
 const cheerio = require('cheerio')
-
-const { USER: user, PASS: pass } = dotenv.parsed
-
-async function login() {
-  let page
-  try {
-    const browser = await puppeteer.launch({
-      headless: false,
-      // slowMo: 5 // slow down by 250ms
-    })
-    page = await browser.newPage()
-    await page.goto('https://frontendmasters.com/login/')
-
-    const username = '#username'
-    const password = '#password'
-    const loginButton = '#loginForm > button'
-
-    await page.waitForSelector(username)
-    await page.click(username)
-    await page.type(username, user)
-
-    await page.waitForSelector(password)
-    await page.click(password)
-    await page.type(password, pass)
-
-    await page.click(loginButton)
-
-    await page.waitForNavigation()
-  } catch (err) {
-    console.error(err)
-  }
-  return page
-}
+const { login } = require('./lib/login')
 
 async function scrapeCourse(page) {
   try {
@@ -45,6 +11,8 @@ async function scrapeCourse(page) {
     await page.click(course)
     await page.waitFor(1 * 1000)
     // await page.waitForNavigation()
+
+    throw 'kopytko'
 
     const forFree = 'body > div.s-vflex-outer > div > section.CourseFrame > div > a.Button.ButtonLarge.ButtonRed'
     await page.click(forFree)
