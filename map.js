af4eb8393df6dc295ae8faebcf24522e91061717
syzer3@gmail.com <syzer3@gmail.com>
2017-12-06 19:51:14 +0100

    formatting

--- map.js
+++ map.js
@@ -3,11 +3,11 @@ const cheerio = require('cheerio')
 const file = fs.readFileSync(process.argv[2], 'utf8')
 const $ = cheerio.load(file)
 const text = $('div.title')
-.map((i, el) => $(el).text()).get()
-.map((s, i) => ({ [i]: s.trim() }))
-.reduce((b, c) => ({
-  ...b,
-  ...c,
-}))
+  .map((i, el) => $(el).text()).get()
+  .map((s, i) => ({ [i]: s.trim() }))
+  .reduce((b, c) => ({
+    ...b,
+    ...c,
+  }))
 
 console.log(text)
